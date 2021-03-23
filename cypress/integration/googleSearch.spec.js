dc98d0e7c367128bc153073db80a9dbe07404830
Prasad Koduru <prasad.k@myplanet.com>
2021-03-23 13:25:07 -0400

    updated google search spec

--- cypress/integration/googleSearch.spec.js
+++ cypress/integration/googleSearch.spec.js
@@ -2,7 +2,7 @@ describe('sample test', function () {
   it('opens google and search for text', function () {
     cy.visit('/')
     cy.fixture('searchTerms').then((searchTerms) => {
-      cy.get('input[nae="q"]').type(searchTerms.searchTerm1)
+      cy.get('input[name="q"]').type(searchTerms.searchTerm1)
     })
     cy.get('input[name="btnK"]', { timeout: 1000 }).first().click({ force: true })
     cy.percySnapshot()
