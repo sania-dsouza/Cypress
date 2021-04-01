d25ef0c4b548c122e4e071ae1e433643d9638a6e
Prasad Koduru <prasad.k@myplanet.com>
2021-04-01 11:43:31 -0400

    updated retry steps

--- cypress/integration/googleSearch.spec.js
+++ cypress/integration/googleSearch.spec.js
@@ -1,10 +1,23 @@
-describe('sample test', function () {
-  it('opens google and search for text', function () {
-    cy.visit('/')
-    cy.fixture('searchTerms').then((searchTerms) => {
-      cy.get('input[name="q"]').type(searchTerms.searchTerm1)
-    })
-    cy.get('input[name="btnK"]', { timeout: 1000 }).first().click({ force: true })
-    cy.percySnapshot()
+describe('sample test', /* {
+  // Customizing retry attempts for a suite of tests
+  retries: {
+    runMode: 2,
+    openMode: 1
+  }
+}, */ function () {
+    it('opens google and search for text', /* {
+    // Customize retry attempts for an individual test
+    retries: {
+      runMode: 2,
+      openMode: 1
+    }
+  }, */
+      function () {
+        cy.visit('/')
+        cy.fixture('searchTerms').then((searchTerms) => {
+          cy.get('input[name="q"]').type(searchTerms.searchTerm1)
+        })
+        cy.get('input[name="btnK"]', { timeout: 1000 }).first().click({ force: true })
+        cy.percySnapshot()
+      })
   })
-})
